a8976174f43e87342af6d50506085e00ace949ff
Sergio Joel Magnacco <smagnacco@despegar.com>
2020-01-16 17:06:48 -0300

    Error handling

---
+++ src/main/scala/edu/training/fun/_6_Errors.scala
@@ -0,0 +1,37 @@
+package edu.training.fun
+import org.omg.CORBA.portable.UnknownException
+
+import scala.util.Try
+import scala.util.control.Exception._
+
+object _6_Errors extends App with BombDeactivator {
+
+  val bomb = new Bomb(false)
+
+  pray(bomb.explotion())
+
+  // try
+  val tryingResult = trying(bomb.explotion())
+
+  // Try
+  val betterTryingResult = tryingBetter(bomb.explotion())
+
+  //Either
+  eitherIsRight(bomb.explotion())
+
+}
+
+class Bomb(explote: Boolean) {
+  def explotion(): Int = if(!explote) 0 else throw UnknownException
+}
+
+trait BombDeactivator {
+
+  def pray[T](thunk: => T): T = thunk
+
+  def trying[T](thunk: => T): T = thunk
+
+  def tryingBetter[T](thunk: => T): Try[T] = Try { thunk }
+
+  def eitherIsRight[T](thunk: => T): Either[Exception, Int] = allCatch.either(thunk)
+}
