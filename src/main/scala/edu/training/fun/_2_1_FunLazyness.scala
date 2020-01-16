a9f2ef83df0db1fa0f67d205bcf87a86f1ca58b6
Sergio Joel Magnacco <smagnacco@despegar.com>
2020-01-16 12:39:27 -0300

    Higher Order Functions and Lazyness

---
+++ src/main/scala/edu/training/fun/_2_1_FunLazyness.scala
@@ -0,0 +1,19 @@
+package edu.training.fun
+
+object _2_1_FunLazyness extends App {
+
+  def identity(x: Int): Int = x
+
+  def method_that_receives_a_lazy_expression(thunk: => Int, callThunk: Boolean): Unit =
+    if (callThunk) {
+      println(s"Thunk will be called right now: ${thunk}")
+    } else {
+      println("Thunk has not been evaluated")
+    }
+
+  val resultNotCallingThunk = method_that_receives_a_lazy_expression(identity(10), callThunk = false)
+
+  val resultCallingThunk = method_that_receives_a_lazy_expression(identity(10), callThunk = true)
+
+
+}
--- src/main/scala/edu/training/fun/_2_FunHigherOrder.scala
+++ src/main/scala/edu/training/fun/_2_FunHigherOrder.scala
@@ -1,5 +1,33 @@
 package edu.training.fun
 
-class _2_FunHigherOrder {
+object _2_FunHigherOrder extends App {
+
+  val identity: Int => Int = x => x
+
+  def identity_as_method(x: Int): Int = x
+
+  val sum_as_higher_order_fun: (Int => Int) => Int => Int = thunk1 => x => thunk1(x)
+
+  val higherOrderFunToBeApplied: Int => Int = sum_as_higher_order_fun(identity)
+
+  val higherOrderFunToBeAppliedUsingAMethod: Int => Int = sum_as_higher_order_fun(identity_as_method)
+
+  higherOrderFunToBeApplied(1)
+
+  higherOrderFunToBeAppliedUsingAMethod(1)
+
+  println(
+    s"""Pasando funciones por parametro:
+        identity(1) = ${identity(1)}
+
+        sum_as_higher_order_fun(identity, identity) = ${sum_as_higher_order_fun(identity)} only defined!
+
+        defining a function that use identity function as parameter higherOrderFunToBeApplied(identity) = ${higherOrderFunToBeApplied}
+
+        applying higherOrderFunToBeApplied(1) = ${higherOrderFunToBeApplied(1)}
+
+        using a method as parameter sum_as_higher_order_fun(identity_as_method) = ${higherOrderFunToBeAppliedUsingAMethod(1)}
+
+       """)
 
 }
